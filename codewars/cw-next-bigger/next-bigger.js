b92f7da2b573cf1b529ded1b8d0bbb95c06f2e9c
Arjun Kahlon <arjunskahlon@gmail.com>
2023-02-28 08:23:01 +0000

    Updated the challenge to determine the next biggest number rather than the largest number that can be created with a rearrangement of the parameters digits.

--- codewars/cw-next-bigger/next-bigger.js
+++ codewars/cw-next-bigger/next-bigger.js
@@ -1,9 +1,25 @@
 /* eslint-disable no-unused-vars */
 
 function nextBigger(n) {
-  const nextNum = parseInt(n.toString().split('').sort((a, b) => b - a).join(''));
 
-  return nextNum >= n
-    ? nextNum
+  if (n < 10) {
+    return -1;
+  }
+
+  const arr = n.toString().split('');
+
+  for (let i = arr.length - 1; i > 0; i--) {
+    if (arr[i] > arr[i - 1]) {
+      const temp = arr[i];
+      arr[i] = arr[i - 1];
+      arr[i - 1] = temp;
+      break;
+    }
+  }
+
+  const biggerNum = parseInt(arr.join(''));
+
+  return biggerNum >= n
+    ? biggerNum
     : -1;
 }
